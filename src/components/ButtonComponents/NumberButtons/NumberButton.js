e18bff572b4a113e9b8541cf60fd1c0d173b515e
nathan-saygers <nathan.saygers@gmail.com>
2019-11-05 13:45:02 -0800

    initial commit

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const NumberButton = () => {
   return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+    <div>
+      <button className="number-button"></button>
+    </div>
   );
 };
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,4 +1,6 @@
 import React from "react";
+import NumbersButton from "../NumberButtons/NumberButton";
+import {numbers} from "../../../data";
 
 //import any components needed
 // example of import from data.js. Note all the ../   This is how we move through folders. 
